7fae6672acc813c3748c13930b28f461b256697d
san2op <139362761+san2op@users.noreply.github.com>
2023-07-28 13:32:23 +0530

    Add files via upload

--- rock-paper-scissor/myScript.js
+++ rock-paper-scissor/myScript.js
@@ -1,11 +1,22 @@
+let computerScore = 0;
+let playerScore = 0;
 
+
+// adding UI //assigning DOM to variables
+
+var rockButton = document.querySelector('.rock');
+var paperbutton = document.querySelector('.paper');
+var scissorsButton = document.querySelector('.scissors');
+var outcomeDiv = document.querySelector('.outcome');
+var playerScoreSpan = document.querySelector('.playerScore');
+var computerScoreSpan = document.querySelector('.computerScore');
 
 function getComputerChoice(){
    let choice = ["rock", "paper", "scissors"];
-    let random = choice[Math.floor(Math.random() * choice.length)]; 
+   let random = choice[Math.floor(Math.random() * choice.length)]; 
    return random;
 }
-console.log(getComputerChoice())
+
 
 //The function computerChoice() first creates an array of the three possible choices for the computer
 // to make: rock, paper, or scissors. It then uses the Math.floor() function to generate a random number 
@@ -14,54 +25,106 @@ console.log(getComputerChoice())
 //The console.log() statement at the end of the function prints the randomly selected choice to the console.
 
 function playOneRound(playerSelection,computerSelection){
-if (playerSelection === computerSelection)
-    return ("it adraw");
-
+if (playerSelection === computerSelection){
+const p = document.createElement('p');
+ p.classList.add('para');
+ p.innerText = "it adraw"
+    outcomeDiv.appendChild(p)
+}
  else if ((playerSelection==="rock") && (computerSelection==="paper")) {
- return("computer win")
+  computerScore++
+  const p = document.createElement('p')
+   p.classList.add('para')
+   p.innerText = "You lose! Paper beats rock"
+      outcomeDiv.appendChild(p)
 }
  else if ((playerSelection==="paper" )&& (computerSelection==="scissors") ){
- return("computer win")
+  computerScore++
+  const p = document.createElement('p')
+  p.classList.add('para')
+  p.innerText = "You lose! scissors beats paper"
+     outcomeDiv.appendChild(p)
 }
  else if((playerSelection==="scissors") && (computerSelection==="rock")) {
- return("computer  win")
+  computerScore++
+  const p = document.createElement('p')
+   p.classList.add('para')
+   p.innerText = "You lose! rock beats scissors"
+      outcomeDiv.appendChild(p)
 }
 else if((playerSelection==="rock") && (computerSelection==="scissors")) {
-    return("you win")
+  playerScore++
+  const p = document.createElement('p')
+   p.classList.add('para')
+   p.innerText = "You win! rock beats scissors"
+       outcomeDiv.appendChild(p)
    }
 else if((playerSelection==="paper") && (computerSelection==="rock")) {
-    return("you win")
+  playerScore++
+  const p = document.createElement('p')
+   p.classList.add('para')
+   p.innerText = "You win! paper beats rock"
+       outcomeDiv.appendChild(p)
    }
 else if((playerSelection==="scissors") && (computerSelection==="paper")) {
-    return("you win")
+  playerScore++
+  const p = document.createElement('p')
+  p.classList.add('para')
+  p.innerText = "You win! scissors beats paper"
+      outcomeDiv.appendChild(p)
    }
 }
 
+//adding UI
+
+const updateScore = (playerScore,computerScore) => {
+  playerScoreSpan.innerText = `Player Score: ${playerScore}`
+  computerScoreSpan.innerText = `Computer score: ${computerScore}`
+}
+
+const checkforWinner = (playerScore,computerScore) => {
+  if (playerScore === 5){
+  const h2 = document.createElement('h2')
+  h2.classList.add('playerWin')
+  h2.innerText = `Player won ${playerScore} to ${computerScore} good job Player! --you saved us!!`
+  outcomeDiv.append(h2)
+}
+
+else if(computerScore === 5){
+    const h2 = document.createElement('h2')
+    h2.classList.add('computerWin')
+    h2.innerText = `Player lose ${playerScore} to ${computerScore} We are doomed!!! computer will rule over us!!!!`
+    outcomeDiv.append(h2)
+}
+}
+
+rockButton.addEventListener("click", () => {
+  const computerSelection = getComputerChoice()
+  const playerSelection = 'rock';
+  playOneRound(playerSelection,computerSelection)
+  updateScore(playerScore,computerScore)
+  checkforWinner(playerScore,computerScore)
+});
+
+paperbutton.addEventListener("click", () => {
+  const computerSelection = getComputerChoice()
+  const playerSelection = 'paper';
+  playOneRound(playerSelection,computerSelection)
+  updateScore(playerScore,computerScore)
+  checkforWinner(playerScore,computerScore)
+});
 
 
-let computerSelection = getComputerChoice();
-let playerSelection = prompt("enter your choice:1-rock,2-paper,3-scissors");
-console.log(computerSelection)
-console.log(playerSelection)
-// alert(playOneRound(playerSelection,computerSelection))
-alert(playOneRound(playerSelection,computerSelection))
-playOneRound()
-playOneRound()
-playOneRound()
-playOneRound()
-playOneRound()
+scissorsButton.addEventListener("click", () => {
+  const computerSelection = getComputerChoice()
+  const playerSelection = 'scissors';
+  playOneRound(playerSelection,computerSelection)
+  updateScore(playerScore,computerScore)
+  checkforWinner(playerScore,computerScore)
+});
 
 
-//this is not correct find a solution to this .you are trying to make sure that some messege 
-//is display to the user when the player is entering  input other than rock,paper,scissors.
-switch (playerSelection) {
-    case "rock":
-      break;
-    case "paper":
-      break;
-    case "scissors":
-      break;
-    default:
-      alert("The user's input is not valid.")
-      break;
-  }
+
+
+
+
